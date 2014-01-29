9bb736fc76a492c9522e9193644a9e77c0bcc618
Pierre Bertet <bonjour@pierrebertet.net>
2014-01-29 15:55:27 +0000

    Better demo

--- example/index.js
+++ example/index.js
@@ -2,73 +2,39 @@ var navbar = require('../');
 var app = angular.module('app', ['navigationBar']);
 
 app.controller('IndexCtrl', function($scope) {
-  var screens = [
-    'Settings',
-    'Groups',
-    'MyGroup',
-    'Person'
-  ];
-  var currentScreen = 0;
 
-  function onAction() {
-    if (currentScreen < screens.length-1) currentScreen++;
-    $scope.navigationBarStatus = navbarStatus(
-      screens[currentScreen],
-      'Edit',
-      (currentScreen > 0)? screens[currentScreen-1] : null,
-      'rtl'
-    );
-  }
-  function onBack() {
-    if (currentScreen > 0) currentScreen--;
-    $scope.navigationBarStatus = navbarStatus(
-      screens[currentScreen],
-      'Edit',
-      (currentScreen > 0)? screens[currentScreen-1] : null,
-      'ltr'
-    );
-  }
+  var baseStatus = {
+    backLabel: null,
+    actionLabel: 'Next',
+    onBack: function() {
+      var newIndex = statuses.indexOf($scope.status) - 1;
+      if (newIndex > -1) {
+        $scope.status = statuses[newIndex];
+        $scope.status.move = 'ltr';
+        if (newIndex > 0) {
+          $scope.status.backLabel = statuses[newIndex-1].title;
+        }
+      }
+    },
+    onAction: function() {
+      var newIndex = statuses.indexOf($scope.status) + 1;
+      if (newIndex < statuses.length) {
+        $scope.status = statuses[newIndex];
+        $scope.status.move = 'rtl';
+        $scope.status.backLabel = statuses[newIndex-1].title;
+        if (!statuses[newIndex+1]) {
+          $scope.status.actionLabel = null;
+        }
+      }
+    }
+  };
 
-  function navbarStatus(title, action, back, move) {
-    return {
-      title: title,
-      actionLabel: action,
-      backLabel: back,
-      move: move,
-      onAction: onAction,
-      onBack: onBack
-    };
-  }
-
-  $scope.navigationBarStatus = navbarStatus(screens[0], 'Edit', null, 'none');
-
-  $scope.directions = [
-    'rtl',
-    'ltr',
-    'none'
+  var statuses = [
+    angular.extend({title: 'Settings'}, baseStatus),
+    angular.extend({title: 'Groups'}, baseStatus),
+    angular.extend({title: 'MyGroup'}, baseStatus),
+    angular.extend({title: 'Person'}, baseStatus)
   ];
 
-  $scope.statuses = [];
-  for (var i = 0; i < 5; i++) {
-    $scope.statuses.push({
-      backLabel: 'First Title',
-      actionLabel: 'Action',
-      title: 'New Title',
-      move: $scope.directions[0],
-      onBack: function() {},
-      onAction: function() {}
-    });
-  }
-
-  $scope.reset = function() {
-    $scope.beforeRtl = false;
-    $scope.beforeLtr = false;
-    $scope.showRtl = false;
-    $scope.showLtr = false;
-  };
-  $scope.reset();
-
-  $scope.updateStatus = function(index) {
-    $scope.navigationBarStatus = angular.copy($scope.statuses[index]);
-  };
+  $scope.status = statuses[0];
 });
